5621aa959a3f1421208558fc844964d90b5e0d14
MarvelousChester <90730377+MarvelousChester@users.noreply.github.com>
2021-09-30 17:04:27 -0400

    Update Black Jack Text Game.py

--- Python Learning Projects/Black Jack Text Game.py
+++ Python Learning Projects/Black Jack Text Game.py
@@ -4,15 +4,23 @@
 # for anyone to casually read and understand
 
 import os  # imports windows commands for commandline
+import random
 
 clear = lambda: os.system('cls')  # this will clear the console as to show only one input , to call use clear()
 
 # input for how many players to play against
 while True:
-    players = input("how many players do you want to play against ")
+    players = input("how many players do you want to play against(Max players: 3) ")
+    if players == 1:
+        continue
     try:
         players = int(players)
-        break
+        if players == 1:
+            print("Invalid input, Players have to be more than 1")
+            continue
+        else:
+            clear()
+            break
     except:
         clear()
         print("invalid input,retry")
@@ -33,25 +41,50 @@ while True:
         break
 print("There are " + str(players) + " Players")
 print("Deck size is:" + str(deck_size))
+clear()
 
-# special cards value
-counter = 0
-ace = 1 or 11  # tricky as ace values differs depending on what card player has COME BACK TO THIS AND Deal with that
-jack = 11
-queen = 12
-king = 13
-# list for cards which depended on the previous answer on deck size
+# Deck and creating sample size for 312 deck
 if deck_size == 312:
-    card_list = ([ace, 2, 	3, 	4, 	5, 	6, 	7, 8, 9, 10, jack, queen, king] * 4) * 6  # makes list to 312 cards
+    deck = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    suit1 = (["Spades"] * 13) * 6
+    suit2 = (["Diamonds"] * 13) * 6
+    suit3 = (["Hearts"] * 13) * 6
+    suit4 = (["Clubs"] * 13) * 6
+    total_suits = suit1 + suit2 + suit3 + suit4
+    # Drawing card move elsewhere
+    v = random.randint(1, 13)
+    suit_pick = random.sample(total_suits, k=1)
+    print("I got a " + str(suit_pick[0]) + " " + str(v))
 else:
-    card_list = [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king] * 4  # makes deck into 52 cards
-for i in card_list:   # small counter to for testing, REmove after
-    counter += 1
-print(card_list)
-print(counter)
+    suit1 = (["Spades"] * 13)
+    suit2 = (["Diamonds"] * 13)
+    suit3 = (["Hearts"] * 13)
+    suit4 = (["Clubs"] * 13)
+    total_suits = suit1 + suit2 + suit3 + suit4
+
+
 # function to draw cards
+# assiging player hands
+if players == 2:
+    player1_hand = []
+    player2_hand = []
+elif players == 3:
+    player1_hand = []
+    player2_hand = []
+    player3_hand = []
+elif players == 4:
+    player1_hand = []
+    player2_hand = []
+    player3_hand = []
+    player4_hand = []
+
+v = random.randint(1, 13)
+suit_pick = random.sample(total_suits, k=1)
+print("I got a " + str(suit_pick[0]) + " " + str(v))
 # does player want to hit or stand
 
 # number generator from ace to king
 
 # ai
+
+## https://www.youtube.com/watch?v=KzqSDvzOFNA watch this video on RAndom Module
